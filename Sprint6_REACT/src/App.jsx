d650154bc1807f2d543c1e4e2c7cd28106f9be39
JordiAltes <jordalan8@gmail.com>
2023-02-14 23:25:19 +0100

    exercici4 acabat

--- Sprint6_REACT/src/App.jsx
+++ Sprint6_REACT/src/App.jsx
@@ -3,18 +3,28 @@ import { useState } from "react";
 import Escena from "./components/Escena.jsx";
 import frases from "./components/frases";
 
- function App() {
-  
-  
+function App() {
+  const [position, setPosition] = useState(1);
+  function ActiveNext() {
+    setPosition(position >= frases.length ? 1 : position + 1);
+    
+  }
+  function ActiveBack(){
+    setPosition(position <= 1 ? frases.length : position - 1);
+  }
+
   return (
     <div>
-      <button className="button" onClick={() => ActiveBack()}>Anterior</button>
-      <button className="button" onClick={() => ActiveNext()}>Següent</button>
-      {frases.map(item => <Escena 
-      key={item.id} 
-      frase={item.text}/>)}
+      <button className="button" onClick={() => ActiveBack()}>
+        Anterior
+      </button>
+      <button className="button" onClick={() => ActiveNext()}>
+        Següent
+      </button>
+      {frases.map((item) => (
+        <Escena key={item.id} frase={item.text} isPainted={position === item.id} />
+      ))}
     </div>
-    )
-  
+  );
 }
-export default App
+export default App;
